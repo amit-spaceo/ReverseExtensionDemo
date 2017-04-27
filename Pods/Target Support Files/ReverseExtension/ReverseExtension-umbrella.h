e20472bbcd0ebc4be6e885afa4ff342d71c3267e
Spaceo Technologies <amitm.spaceo@gmail.com>
2017-04-27 17:53:50 +0530

    Source

---
+++ Pods/Target Support Files/ReverseExtension/ReverseExtension-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DelegateTransporter.h"
+#import "ExceptionHandler.h"
+
+FOUNDATION_EXPORT double ReverseExtensionVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReverseExtensionVersionString[];
+
